cb5fd74627e1941163829dda225980ef0a9e7ccf
gary <gehang_it@163.com>
2017-12-29 14:17:23 +0800

    增加提取渠道的方法

--- app/src/main/java/com/mayinews/g/app/MyApplication.java
+++ app/src/main/java/com/mayinews/g/app/MyApplication.java
@@ -1,7 +1,14 @@
 package com.mayinews.g.app;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.text.TextUtils;
 import android.util.Log;
+
+import com.umeng.analytics.MobclickAgent;
+import com.umeng.commonsdk.UMConfigure;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,12 +33,27 @@ public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
+
         super.onCreate();
 //        setupLeakCanary();
         String appInfo = getAppInfo();
         Log.e("TAG","包名"+appInfo);
+/**
+ * 初始化common库
+ * 参数1:上下文，不能为空
+ * 参数2:设备类型，UMConfigure.DEVICE_TYPE_PHONE为手机、UMConfigure.DEVICE_TYPE_BOX为盒子，默认为手机
+ * 参数3:Push推送业务的secret
+ */
+        UMConfigure.init(this, UMConfigure.DEVICE_TYPE_PHONE, "");
+        UMConfigure.setLogEnabled(true);
+        UMConfigure.setEncryptEnabled(true);
+        MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_UM_NORMAL);
 
-
+        /**
+         * 在需要的地方调用上述方法
+         */
+        String channelNumber = getAppMetaData(getBaseContext(), "UMENG_CHANNEL");//获取app当前的渠道号
+        Log.e("TAG","渠道"+channelNumber);
     }
 
 //    protected RefWatcher setupLeakCanary() {
@@ -54,6 +76,33 @@ public class MyApplication extends Application {
         } catch (Exception ignored) {
         }
         return null;
+    }
+
+
+    /**
+     * 获取app当前的渠道号或application中指定的meta-data
+     *
+     * @return 如果没有获取成功(没有对应值，或者异常)，则返回值为空
+     */
+    public static String getAppMetaData(Context context, String key) {
+        if (context == null || TextUtils.isEmpty(key)) {
+            return null;
+        }
+        String channelNumber = null;
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            if (packageManager != null) {
+                ApplicationInfo applicationInfo = packageManager.getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+                if (applicationInfo != null) {
+                    if (applicationInfo.metaData != null) {
+                        channelNumber = applicationInfo.metaData.getString(key);
+                    }
+                }
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return channelNumber;
     }
 }
 
